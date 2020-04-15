8328412e5efb399b7c4a87f030ba60e1446922cd
ibparrague@miuandes.cl <ibparrague@miuandes.cl>
2020-04-15 16:48:58 -0400

    Fixed Menu woring once Fixed Deserialize Printing more information on Load

--- PreEntregaProjecto1SoftwareDesign/Program.cs
+++ PreEntregaProjecto1SoftwareDesign/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace SDPreSubmissionNS
@@ -14,18 +16,29 @@ namespace SDPreSubmissionNS
 
         static void Main(string[] args)
         {
-            StartMenu();
-            if (option.Equals('1'))
-            {
-                blocks = GatherBlocks(path);
-                SerializeBlocks();
-            }
-            else if (option.Equals('2'))
-            {
-                DeserializeBlocks();
-                PrintBlocks();
-            }
+            bool menu = true;
 
+            while (menu)
+            {
+                StartMenu();
+                if (option.Equals("1"))
+                {
+                    blocks = GatherBlocks(path);
+                    SerializeBlocks();
+                }
+                else if (option.Equals("2"))
+                {
+                    if (DeserializeBlocks())
+                    {
+                        PrintBlocks();
+                    }
+                }
+                else if (option.Equals("3"))
+                {
+                    menu = false;
+                }
+            }
+            Console.WriteLine("Bye bye");
             Console.ReadLine();
         }
 
@@ -66,7 +79,6 @@ namespace SDPreSubmissionNS
                                 #endregion
                                 Block block = new Block(id, x, y, z, tonn, au, cu, porc_profit);
                                 blocks.Add(block);
-                                Console.WriteLine(block.id);
                             }
 
                         }
@@ -74,7 +86,6 @@ namespace SDPreSubmissionNS
                 }
             }
             Console.WriteLine("Done gathering blocks.");
-            //Console.ReadLine();
             return blocks;
         }
 
@@ -87,17 +98,32 @@ namespace SDPreSubmissionNS
             Console.WriteLine("Done saving Block Model.");
         }
 
-        static public void DeserializeBlocks()
+        //retorna True su funciona y False si falla
+        static public bool DeserializeBlocks()
         {
-            stream = new FileStream(@newPath,FileMode.Open,FileAccess.Read);
-            blocks = (Block)formatter.Deserialize(stream);
+            IFormatter formatter = new BinaryFormatter();
+            if (File.Exists(@newPath))
+            {
+                Stream stream = new FileStream(@newPath, FileMode.Open, FileAccess.Read);
+                blocks = (List<Block>)formatter.Deserialize(stream);
+                stream.Close();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"File not found in {@newPath}");
+                return false;
+            }
+            
         }
 
         static public void PrintBlocks()
         {
+            Console.WriteLine("Blocks information:");
             foreach (Block block in blocks)
             {
-                Console.WriteLine(block.id);
+                Console.Write($"ID: {block.id}, x:{block.x}, y:{block.y}, z:{block.z}, tonn:{block.tonn}, " +
+                    $"au:{block.au}, cu:{block.cu}, proc_profit:{block.porc_profit} \n");
             }
         }
 
@@ -106,15 +132,16 @@ namespace SDPreSubmissionNS
             Console.WriteLine("Welcome, please select an option: ");
             Console.WriteLine("1. Save Block Model.");
             Console.WriteLine("2. Load Block Model.");
+            Console.WriteLine("3. Exit");
             option = Console.ReadLine();
-            if (option.Equals('1'))
+            if (option.Equals("1"))
             {
                 Console.Write("Please enter the path of your Block Model file:");
                 path = Console.ReadLine();
                 Console.Write("Please enter the name of the file you want to save: ");
                 newPath = Console.ReadLine();
             }
-            else if (option.Equals('2'))
+            else if (option.Equals("2"))
             {
                 Console.Write("Please enter the name of the file you want to load:");
                 newPath = Console.ReadLine();
