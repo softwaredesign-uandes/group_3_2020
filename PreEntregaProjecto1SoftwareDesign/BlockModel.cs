cac709db8a6a05e8214df817ca31af241dcbdcb5
Gustavo Vicuña <gpvicuna@miuandes.cl>
2020-04-22 16:21:11 -0400

    feat(BlockModel): add name

--- PreEntregaProjecto1SoftwareDesign/BlockModel.cs
+++ PreEntregaProjecto1SoftwareDesign/BlockModel.cs
@@ -7,10 +7,12 @@ namespace SDPreSubmissionNS
     [Serializable]
     public class BlockModel
     {
+        public string name;
         public List<Block> blocks { get; set; }
 
-        public BlockModel(List<Block> blocks)
+        public BlockModel(List<Block> blocks, string name)
         {
+            this.name = name;
             this.blocks = blocks;
         }
 
