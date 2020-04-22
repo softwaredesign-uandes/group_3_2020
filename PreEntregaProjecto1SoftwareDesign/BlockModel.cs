20c92c957774e51ceadf854c16b33085283fc7db
Gustavo Vicuña <gpvicuna@miuandes.cl>
2020-04-22 16:04:50 -0400

    feat(BlockModel): create class

---
+++ PreEntregaProjecto1SoftwareDesign/BlockModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDPreSubmissionNS
+{
+    [Serializable]
+    public class BlockModel
+    {
+        public List<Block> blocks { get; set; }
+
+        public BlockModel(List<Block> blocks)
+        {
+            this.blocks = blocks;
+        }
+    }
+}
