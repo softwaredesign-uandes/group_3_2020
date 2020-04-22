d612f82fe03129065a21cdf4bca3f3604fd7921a
ibparrague@miuandes.cl <ibparrague@miuandes.cl>
2020-04-22 17:40:24 -0400

    Cambiado Serializacion a archivo aparte, además de guardar y cargar BlockModels en vez de Blocks

--- PreEntregaProjecto1SoftwareDesign/Program.cs
+++ PreEntregaProjecto1SoftwareDesign/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
+using System.Linq;
 
 namespace SDPreSubmissionNS
 {
@@ -12,21 +12,27 @@ namespace SDPreSubmissionNS
         static private string path;
         static private string newPath;
         static private string option;
-        static private List<Block> blocks;
+        static private List<BlockModel> blockModels;
 
         static void Main(string[] args)
+        {
+            blockModels = new List<BlockModel>();
+            MainMenu();
+        }
+
+        static void MainMenu()
         {
             bool menu = true;
 
             while (menu)
             {
-                if (blocks == null)
+                if (blockModels.Count == 0)
                 {
                     Console.WriteLine("database not loaded");
                 }
                 else
                 {
-                    Console.WriteLine($"database loaded {path}");
+                    Console.WriteLine($"database loaded {blockModels[0].name}");
                 }
                 Console.WriteLine("Welcome, please select an option: ");
                 Console.WriteLine("1. Save Block Model.");
@@ -39,33 +45,51 @@ namespace SDPreSubmissionNS
                 {
                     Console.Write("Please enter the path of your Block Model file:");
                     path = Console.ReadLine();
-                    Console.Write("Please enter the name of the file you want to save: ");
-                    newPath = Console.ReadLine();
-                    blocks = BlockLoaders.GatherBlocks(path);
-                    SerializeBlocks();
+                    if (File.Exists(path))
+                    {
+                        FileInfo file = new FileInfo(path);
+
+                        Console.Write("Please enter the name of the file you want to save: ");
+                        newPath = Console.ReadLine();
+
+                        List<Block> blocks = new List<Block>();
+                        blocks = BlockLoaders.GatherBlocks(path);
+                        BlockModel blockModel = new BlockModel(blocks,file.Name);
+                        BlockSerializer.SerializeBlockModel(newPath, blockModel);
+                    }
+                    else
+                    {
+                        Console.WriteLine("File not found");
+                    }
                 }
                 else if (option.Equals("2"))
                 {
                     Console.Write("Please enter the name of the file you want to load:");
                     newPath = Console.ReadLine();
-                    if (DeserializeBlocks())
+                    BlockModel blockModel = BlockSerializer.DeserializeBlockModel(newPath);
+                    if (blockModel != null)
                     {
+                        blockModels.Add(blockModel);
                         PrintBlocks();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error deserializing Block Model");
                     }
                 }
                 else if (option.Equals("3"))
                 {
-                    if (blocks != null)
+                    if (blockModels.Count != 0)
                     {
-                        Console.WriteLine("Enter Block id:");
+                        Console.WriteLine("(HAY QUE ARREGLAR ESTO) Enter Block id:");
                         option = Console.ReadLine();
-                        PrintBlockById(option, blocks);
+                        //PrintBlockById(option, blocks);
                     }
                     else
                     {
                         Console.WriteLine("Please load a database first");
                     }
-                    
+
                 }
                 else if (option.Equals("4"))
                 {
@@ -101,41 +125,13 @@ namespace SDPreSubmissionNS
                 Console.WriteLine("No int detected, going back to menu");
             }
         }
-        static public void SerializeBlocks()
-        {
-            IFormatter formatter = new BinaryFormatter();
-            Stream stream = new FileStream(@newPath,FileMode.Create,FileAccess.Write);
-            formatter.Serialize(stream, blocks);
-            stream.Close();
-            Console.WriteLine("Done saving Block Model.");
-        }
-
-        //retorna True su funciona y False si falla
-        static public bool DeserializeBlocks()
-        {
-            IFormatter formatter = new BinaryFormatter();
-            if (File.Exists(@newPath))
-            {
-                Stream stream = new FileStream(@newPath, FileMode.Open, FileAccess.Read);
-                blocks = (List<Block>)formatter.Deserialize(stream);
-                stream.Close();
-                return true;
-            }
-            else
-            {
-                Console.WriteLine($"File not found in {@newPath}");
-                return false;
-            }
-            
-        }
 
         static public void PrintBlocks()
         {
             Console.WriteLine("Blocks information:");
-            foreach (Block block in blocks)
+            foreach (Block block in blockModels[0].blocks)
             {
-                Console.Write($"ID: {block.id}, x:{block.x}, y:{block.y}, z:{block.z}, tonn:{block.tonn}, " +
-                    $"au:{block.au}, cu:{block.cu}, proc_profit:{block.porc_profit} \n");
+                Console.Write($"ID: {block.id}, x:{block.x}, y:{block.y}, z:{block.z} \n");
             }
         }
 
