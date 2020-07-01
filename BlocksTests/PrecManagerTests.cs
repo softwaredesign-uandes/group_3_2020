fbee198bdebb6e1230a395f95a742c9424092eed
barracius <rjgonzalez6@gmail.com>
2020-06-30 23:00:59 -0400

    Extract Tests Done

--- BlocksTests/PrecManagerTests.cs
+++ BlocksTests/PrecManagerTests.cs
@@ -44,8 +44,80 @@ namespace BlocksTests
             correctDictionary.Add(4, l4);
             correctDictionary.Add(5, l5);
             Dictionary<int, List<int>> evaluatedDictionary = PrecManager.GenerateDictionary("Test Files/GenerateDictionaryTestFile.txt");
-            bool dictionariesEqual = correctDictionary.Keys.Count == evaluatedDictionary.Keys.Count && correctDictionary.Keys.All(k => evaluatedDictionary.ContainsKey(k) && evaluatedDictionary[k].All(correctDictionary[k].Contains) && evaluatedDictionary[k].Count == correctDictionary[k].Count); //evaluatedDictionary[k].All(correctDictionary[k].Contains) && evaluatedDictionary[k].Count == correctDictionary[k].Count;
+            bool dictionariesEqual = correctDictionary.Keys.Count == evaluatedDictionary.Keys.Count && correctDictionary.Keys.All(k => evaluatedDictionary.ContainsKey(k) && evaluatedDictionary[k].All(correctDictionary[k].Contains) && evaluatedDictionary[k].Count == correctDictionary[k].Count);
             Assert.IsTrue(dictionariesEqual);
+        }
+
+        //Method Tested: ExtractBlock
+        //Context: Passing the following dictionary file:
+        //{
+        //  0: []
+        //  1: [2, 9]
+        //  2: [3, 10]
+        //  3: [4, 11]
+        //  4: [5]
+        //  5: [0, 6]
+        //}
+        //Expectation: Extract the block 0, should return:
+        //
+        //  [0]
+        //
+        [TestMethod]
+        public void ExtractBlockNullTest()
+        {
+            List<int> l0 = new List<int>();
+            List<int> l1 = new List<int>(new int[] { 2, 9 });
+            List<int> l2 = new List<int>(new int[] { 3, 10 });
+            List<int> l3 = new List<int>(new int[] { 4, 11 });
+            List<int> l4 = new List<int>(new int[] { 5 });
+            List<int> l5 = new List<int>(new int[] { 0, 6 });
+            Dictionary<int, List<int>> dictionary = new Dictionary<int, List<int>>();
+            dictionary.Add(0, l0);
+            dictionary.Add(1, l1);
+            dictionary.Add(2, l2);
+            dictionary.Add(3, l3);
+            dictionary.Add(4, l4);
+            dictionary.Add(5, l5);
+            List<int> correctList = new List<int>(new int[] { 0 }); 
+            List<int> evaluatedList = PrecManager.ExtractBlock(0,dictionary);
+            bool listEqual = correctList.All(evaluatedList.Contains) && correctList.Count == evaluatedList.Count;
+            Assert.IsTrue(listEqual);
+        }
+
+        //Method Tested: ExtractBlock
+        //Context: Passing the following dictionary file:
+        //{
+        //  0: []
+        //  1: [2, 3]
+        //  2: [3, 5]
+        //  3: [1, 5]
+        //  4: [5]
+        //  5: [0]
+        //}
+        //Expectation: Extract the block 1, should return:
+        //
+        //  [0, 1, 2, 3, 5]
+        //
+        [TestMethod]
+        public void ExtractBlockRepeatedValuesTest()
+        {
+            List<int> l0 = new List<int>();
+            List<int> l1 = new List<int>(new int[] { 2, 3 });
+            List<int> l2 = new List<int>(new int[] { 3, 5 });
+            List<int> l3 = new List<int>(new int[] { 1, 5 });
+            List<int> l4 = new List<int>(new int[] { 5 });
+            List<int> l5 = new List<int>(new int[] { 0 });
+            Dictionary<int, List<int>> dictionary = new Dictionary<int, List<int>>();
+            dictionary.Add(0, l0);
+            dictionary.Add(1, l1);
+            dictionary.Add(2, l2);
+            dictionary.Add(3, l3);
+            dictionary.Add(4, l4);
+            dictionary.Add(5, l5);
+            List<int> correctList = new List<int>(new int[] { 0, 1, 2, 3, 5 });
+            List<int> evaluatedList = PrecManager.ExtractBlock(1, dictionary);
+            bool listEqual = correctList.All(evaluatedList.Contains) && correctList.Count == evaluatedList.Count;
+            Assert.IsTrue(listEqual);
         }
     }
 }
